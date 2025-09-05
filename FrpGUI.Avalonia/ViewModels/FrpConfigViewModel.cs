3196a8ac2dbbff0fb31a92cc0098dcafe97be74e
autodotua <autodotua@outlook.com>
2025-09-05 15:34:32 +0800

    优化客户端规则界面

--- FrpGUI.Avalonia/ViewModels/FrpConfigViewModel.cs
+++ FrpGUI.Avalonia/ViewModels/FrpConfigViewModel.cs
@@ -6,6 +6,7 @@ using FrpGUI.Models;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using FrpGUI.Avalonia.Factories;
 using FzLib.Avalonia.Dialogs;
@@ -20,6 +21,8 @@ public partial class FrpConfigViewModel : ViewModelBase
     [ObservableProperty]
     private ObservableCollection<Rule> rules;
 
+    public static readonly string AddButtonDataName = Guid.NewGuid().ToString();
+
     /// <inheritdoc/>
     public FrpConfigViewModel(IDataProvider provider,
         DialogFactory dialogFactory,
@@ -27,12 +30,13 @@ public partial class FrpConfigViewModel : ViewModelBase
     {
     }
 
+    [RelayCommand]
     public async Task AddRuleAsync()
     {
         var result = await DialogService.ShowCustomDialogAsync<Rule>(DialogFactory.CreateRuleDialog());
         if (result is Rule newRule)
         {
-            Rules.Add(newRule);
+            Rules.Insert(Rules.Count - 1, newRule);
         }
     }
 
@@ -42,7 +46,8 @@ public partial class FrpConfigViewModel : ViewModelBase
         if (frp?.Config is ClientConfig cc)
         {
             Rules = new ObservableCollection<Rule>(cc.Rules);
-            Rules.CollectionChanged += (s, e) => cc.Rules = [.. Rules];
+            Rules.Add(new Rule() { Name = AddButtonDataName });
+            Rules.CollectionChanged += (s, e) => cc.Rules = Rules.Take(Rules.Count - 1).ToList();
         }
     }
 
